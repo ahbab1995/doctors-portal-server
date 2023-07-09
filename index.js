e8d1f0e1134932af45e3d920fdc076365a1b8054
ahbab1995 <islamahbab1996@yahoo.com>
2023-07-10 01:06:47 +0600

    patient booking verify  new code added

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yeu24qk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-   
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('booking');
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.post('/booking',async(req,res)=>{
+      const booking = req.body;
+      const query = {treatment:booking.treatment, data:booking.data, patient:booking.patient}
+      const exists = await bookingCollection.findOne(query)
+      if(exists){
+        return res.send({success:false,booking:exists})
+      }
+      const result = await bookingCollection.insertOne(booking)
+      res.send({success:true,result})
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,13 +48,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('doctors portal')
-})
+app.get("/", (req, res) => {
+  res.send("doctors portal");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
