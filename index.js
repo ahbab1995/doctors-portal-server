34f6390b52469b1b61a356eca7eca8f2c033190e
ahbab1995 <islamahbab1996@yahoo.com>
2023-07-13 22:23:16 +0600

    Save Registered user information in the database

--- index.js
+++ index.js
@@ -12,51 +12,82 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yeu24qk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const serviceCollection = client.db('doctors_portal').collection('services');
-    const bookingCollection = client.db('doctors_portal').collection('booking');
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client.db("doctors_portal").collection("booking");
+    const userCollection = client.db("doctors_portal").collection("user");
 
-    app.get('/service', async (req, res) => {
+    app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
 
-    app.post('/booking',async(req,res)=>{
-      const booking = req.body;
-      const query = {treatment:booking.treatment, data:booking.data, patient:booking.patient}
-      const exists = await bookingCollection.findOne(query)
-      if(exists){
-        return res.send({success:false,booking:exists})
-      }
-      const result = await bookingCollection.insertOne(booking)
-      res.send({success:true,result})
-    })
-
-    app.get('/available', async(req,res)=>{
+    app.get("/available", async (req, res) => {
       const date = req.query.date;
       const services = await serviceCollection.find().toArray();
 
-      const query = {date:date}
+      const query = { date: date };
       const booking = await bookingCollection.find(query).toArray();
 
-      services.forEach(service => {
-        const serviceBooking = booking.filter(b => b.treatment === service.name)
-        const booked =serviceBooking.map(s=>s.slot)
-        const available = service.slots.filter(s=>!booked.includes(s))
-        service.available = available; 
-      })
-      res.send(services)
-    })
+      services.forEach((service) => {
+        const serviceBooking = booking.filter(
+          (b) => b.treatment === service.name
+        );
+        const booked = serviceBooking.map((s) => s.slot);
+        const available = service.slots.filter((s) => !booked.includes(s));
+        service.available = available;
+      });
+      res.send(services);
+    });
+
+    app.put("/user/:email", async(req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email:email };
+      const options = { upsert: true };
+      // create a document that sets the plot of the movie
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    });
 
 
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const booking = await bookingCollection.find(query).toArray();
+      res.send(booking);
+    });
 
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        data: booking.data,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      res.send({ success: true, result });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
